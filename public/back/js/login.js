f2de36e028d1a1c717082ed68ee5e3393b8143ed
丁丁rz <dingdingrz@163.com>
2018-04-06 21:48:43 +0800

    登录验证完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -22,6 +22,9 @@ $(function(){
                     max:6,
                     min:2,
                     message:"用户名长度必须是2-6位"
+                  },
+                  callback:{
+                    message:"用户名不存在"
                   }
                 }
             },
@@ -34,6 +37,9 @@ $(function(){
                   max:6,
                   min:2,
                   message:"用户名长度必须是2-6位"
+                },
+                callback:{
+                  message:"密码错误"
                 }
               }
             }
@@ -48,7 +54,20 @@ $(function(){
       url:'/employee/employeeLogin',
       success:function(info){
         console.log(info);
+        if(info.success){
+          location.href="index.html"
+        }
+        if(info.error===1000){
+          $("#form").data("bootstrapValidator").updateStatus("username","INVALID","callback");
+        }
+        if(info.error===1001){
+          $("#form").data("bootstrapValidator").updateStatus("password","INVALID","callback");
+        }
       }
     })
+  })
+
+  $("[type='reset']").click(function(){
+    $("#form").data("bootstrapValidator").resetForm()
   })
 })
